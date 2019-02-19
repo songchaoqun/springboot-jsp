a343f14ebacbb12e9ccf11cad4459f675a20df84
songchaoqun <guangwuscq@163.com>
2019-02-19 15:28:10 +0800

    修改文件12

--- src/main/java/com/jk/config/MyBatisConfiguration.java
+++ src/main/java/com/jk/config/MyBatisConfiguration.java
@@ -16,6 +16,7 @@ public class MyBatisConfiguration {
         p.setProperty("rowBoundsWithCount", "true");
         p.setProperty("reasonable", "true");
         pageHelper.setProperties(p);
+        /*pageHelper.setProperties(p);*/
 /*
         return pageHelper;
 */
